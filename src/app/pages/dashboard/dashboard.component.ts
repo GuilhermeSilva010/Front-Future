273936160e7323fb0cbbc1bd2403ca4b00063a62
matheus ferreira <matheusfneves1998@gmail.com>
2021-05-10 22:16:39 -0300

    fodasse

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Paginator } from 'primeng/paginator/paginator';
+import { debounceTime, distinctUntilChanged, merge, switchMap, tap } from 'rxjs/operators';
+import { DashI } from '../dashboard/dashboard.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,12 +11,114 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  dashs: DashI[];
+  dash: DashI;
 
-  constructor() { }
+  form: FormGroup;
+  form2: FormGroup;
+
+  counter: number = 0;
+  @ViewChild("pg", { static: true }) public paginator: Paginator;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    console.log("Passei")
+    this.form = this.fb.group({
+        valor: new FormControl("", [
+            Validators.required
+        ]),
+    });
+
+    this.form2 = this.fb.group({
+        data: new FormControl(""),
+        page: new FormControl(0),
+        rows: new FormControl(10),
+    });
+
+    this.form2
+        .get("data")
+        .valueChanges.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+            tap(() => (this.loading = true)),
+            switchMap(() =>
+                this.siteService.listPage(this.form2.getRawValue())
+            ),
+            tap(() => (this.loading = false))
+        )
+        .subscribe((values) => {
+            this.sites = [];
+            this.paginator.changePage(0);
+            this.getCounter();
+            merge(this.sites, values.data).subscribe((values) =>
+                this.sites.push(values)
+            );
+        });
+
+    this.form2
+        .get("page")
+        .valueChanges.pipe(
+            debounceTime(100),
+            distinctUntilChanged(),
+            tap(() => (this.loading = true)),
+            switchMap(() =>
+                this.siteService.listPage(this.form2.getRawValue())
+            ),
+            tap(() => (this.loading = false))
+        )
+        .subscribe((values) => {
+            this.sites = [];
+            merge(this.sites, values.data).subscribe((values) =>
+                this.sites.push(values)
+            );
+        });
+
+    this.form2
+        .get("rows")
+        .valueChanges.pipe(
+            debounceTime(100),
+            distinctUntilChanged(),
+            tap(() => (this.loading = true)),
+            switchMap(() =>
+                this.siteService.listPage(this.form2.getRawValue())
+            ),
+            tap(() => (this.loading = false))
+        )
+        .subscribe((values) => {
+            this.sites = [];
+            merge(this.sites, values.data).subscribe((values) =>
+                this.sites.push(values)
+            );
+        });
+    this.refresh();
+}
+
+  getCounter() {
+    this.siteService
+        .count(this.form2.getRawValue())
+        .subscribe((value) => (this.counter = value.data));
   }
 
-  
+  paginate(event) {
+    this.form2.patchValue({ page: event.page });
+    this.form2.patchValue({ rows: event.rows });
+}
+
+ refresh() {
+    this.loading = true;
+    this.dashs = [];
+    this.siteService
+        .listPage(this.form2.getRawValue())
+        .subscribe((sites) => {
+            if ((dash as ApiResponseI).code != 200) {
+                this.showDialog(
+                    "Ops, algum erro aconteceu.",
+                    (sites as ApiResponseI).msg
+                );
+            }
+            this.dashs = (dashs as ApiResponseI).data as DashI[];
+            this.getCounter();
+            this.loading = false;
+        });
+  } 
 }
---
+++ src/app/pages/dashboard/dashboard.model.ts
@@ -0,0 +1,8 @@
+export interface DashI {
+  valor: string;
+  name?: string;
+  apliMin: string;
+  priceUni?: string;
+  rentabi: string;
+  taxa?: string;
+}
